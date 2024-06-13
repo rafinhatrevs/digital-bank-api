95e454c8b5d8c6af4795ccbd76a23df8d0dce279
Rafaella Trevizan <rafaella_trevizan@hotmail.com>
2024-06-12 22:04:04 -0300

    Criando estrutura DB

---
+++ src/sql/estrutura.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE digital_bank;
+
+CREATE TABLE contas (
+	id serial primary key,
+    nome varchar NOT NULL,
+    cpf varchar(11) NOT NULL UNIQUE,
+    data_nascimento date NOT NULL,
+    telefone varchar NOT NULL,
+    email varchar NOT NULL UNIQUE,
+    senha varchar(6) NOT NULL
+);
+
+CREATE TABLE depositos (
+	id serial primary key,
+    valor integer NOT NULL,
+    conta_id integer NOT NULL references contas(id)
+);
+
+CREATE TABLE saques (
+	id serial primary key,
+    valor integer NOT NULL,
+    conta_id integer NOT NULL references contas(id)
+);
+
+CREATE TABLE transferencias (
+	id serial primary key,
+    valor integer NOT NULL,
+    conta_origem_id integer NOT NULL references contas(id),
+    conta_destino_id integer NOT NULL references contas(id)
+);
+
+ALTER TABLE contas
+ADD saldo NUMERIC DEFAULT 0;
+
+ALTER TABLE contas
+ALTER COLUMN senha TYPE VARCHAR;
+
+
+
+
+
