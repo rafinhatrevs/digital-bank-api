e8075d24071bf8721dde9c8563cbe78ed3023156
Rafaella Trevizan <rafaella_trevizan@hotmail.com>
2024-06-12 22:15:14 -0300

    Configurações

---
+++ src/configs.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // Porta de acesso ao servidor
+    serverPort: 3000,
+
+    // Senha do JWT 
+    jwtSecret: 'mysecretkey',
+
+    // Configurações do Postgres
+    dbHost: 'localhost',
+    dbPort: 5432,
+    dbName: 'digital_bank',
+    dbUser: 'postgres',
+    dbPassword: '1234',
+}
