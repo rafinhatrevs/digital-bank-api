8e1c0ee2d7d99dcd5b286987905ef34d0e7fd554
Rafaella Trevizan <rafaella_trevizan@hotmail.com>
2024-06-14 23:40:40 -0300

    Aplicando endpoints de atualizar e excluir conta

--- src/controladores/contas.js
+++ src/controladores/contas.js
@@ -17,10 +17,6 @@ const listarContas = async (req, res) => {
 const criarConta = async (req, res) => {
     const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
 
-    if (!nome || !cpf || !data_nascimento || !telefone || !email || !senha) {
-        return res.status(400).json({ mensagem: 'Todos os campos obrigatórios devem ser informados.' });
-    }
-
     try {
         const { rowCount } = await pool.query(`SELECT * FROM contas WHERE cpf = $1 OR email = $2`, [cpf, email]);
 
@@ -35,54 +31,59 @@ const criarConta = async (req, res) => {
 
         return res.status(201).send();
     } catch (error) {
-        console.log(error);
+        //console.log(error);
         return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
     }
 };
 
-/*const atualizarUsuarioConta = async (req, res) => {
-    const conta_encontrada = req.contas;
+const atualizarConta = async (req, res) => {
     const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
 
     try {
-        bancodedados.contas[conta_encontrada].usuario.nome = nome;
-        bancodedados.contas[conta_encontrada].usuario.cpf = cpf;
-        bancodedados.contas[conta_encontrada].usuario.data_nascimento = data_nascimento;
-        bancodedados.contas[conta_encontrada].usuario.telefone = telefone;
-        bancodedados.contas[conta_encontrada].usuario.email = email;
-        bancodedados.contas[conta_encontrada].usuario.senha = senha;
+        const { rowCount } = await pool.query(`SELECT * FROM contas WHERE cpf = $1 OR email = $2`, [cpf, email]);
 
-        const dados_string = `module.exports = ${JSON.stringify(bancodedados)}`;
+        if (rowCount > 0) {
+            return res.status(400).json({ mensagem: 'Já existe uma conta com os dados informados.' });
+        }
 
-        await fs.writeFile('./src/bancodedados.js', dados_string);
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+        await pool.query(`UPDATE contas SET nome = $1, cpf = $2, data_nascimento = $3, telefone = $4, email = $5, senha = $6
+            WHERE id = $7`, [nome, cpf, data_nascimento, telefone, email, senhaCriptografada, req.conta.id]);
 
         return res.status(204).send();
-    } catch (erro) {
-        return res.status(500).json({ "erro": erro.message });
+    } catch (error) {
+        //console.log(error);
+        return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
     }
 };
 
 const excluirConta = async (req, res) => {
-    const conta_encontrada = req.contas;
-
-    if (bancodedados.contas[conta_encontrada].saldo_conta !== 0) {
-        return res.status(400).json({ "mensagem": "A conta só pode ser removida se o saldo for zero!" });
-    }
-
     try {
-        bancodedados.contas.splice(conta_encontrada, 1);
+        const { rows, rowCount } = await pool.query(`SELECT * FROM contas WHERE id = $1`, [req.conta.id]);
 
-        const dados_string = `module.exports = ${JSON.stringify(bancodedados)}`;
+        if (rowCount === 0) {
+            return res.status(404).json({ mensagem: 'Conta não encontrada.' });
+        }
 
-        await fs.writeFile('./src/bancodedados.js', dados_string);
+        const conta = rows[0];
+
+        if (conta.saldo !== 0) {
+            return res.status(400).json({ mensagem: 'Saldo da conta deve estar zerado!' });
+        }
+
+        await pool.query(`DELETE FROM contas WHERE id = $1`, [req.conta.id]);
 
         return res.status(204).send();
-    } catch (erro) {
-        return res.status(500).json({ "erro": erro.message });
+    } catch (error) {
+        //console.log(error);
+        return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
     }
-};*/
+};
 
 module.exports = {
     listarContas,
     criarConta,
+    atualizarConta,
+    excluirConta
 };
